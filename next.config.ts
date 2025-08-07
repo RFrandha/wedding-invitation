16637e11d7e33dba1f9c8acb361361fa37d4b3a7
Restow Frandha <36231718+RFrandha@users.noreply.github.com>
2025-08-07 20:08:58 +0700

    disable caching

--- next.config.ts
+++ next.config.ts
@@ -25,10 +25,10 @@ const nextConfig = {
     },
   },
   // Improved caching
-  cacheHandler: process.env.NODE_ENV === 'production' 
-    ? require.resolve('./cache-handler.js') 
-    : undefined,
-  cacheMaxMemorySize: 0, // disable default in-memory caching
+  // cacheHandler: process.env.NODE_ENV === 'production' 
+  //   ? require.resolve('./cache-handler.js') 
+  //   : undefined,
+  // cacheMaxMemorySize: 0, // disable default in-memory caching
 }
 
 module.exports = nextConfig
