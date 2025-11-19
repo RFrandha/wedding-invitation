aba18b7a1f46154ef8d2c420cf70d7f5aba6d273
restow.frandha <restow.frandha@virgoku.id>
2025-11-19 14:10:53 +0700

    Update Layout Metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,16 +21,25 @@ export const metadata: Metadata = {
     description: "Sabtu, 6 Desember 2025",
     images: [
       {
-        url: "https://photos.rever.cyou/pub-img/cover2.jpg",
-        secureUrl: "https://photos.rever.cyou/pub-img/cover2.jpg",
+        url: "https://photos.rever.cyou/pub-img/opengraph_cover.png",
+        secureUrl: "https://photos.rever.cyou/pub-img/opengraph_cover.png",
         width: 1200,
         height: 630,
         alt: "The Wedding of Verina & Restow",
       },
     ],
     type: "website",
-    url: "https://rever.cyou"
+    url: "https://rever.cyou",
+    siteName: "The Wedding of Verina & Restow",
+    locale: "id_ID",
   },
+  twitter: {
+    card: "summary_large_image",
+    title: "The Wedding of Verina & Restow",
+    description: "Sabtu, 6 Desember 2025",
+    images: ["https://photos.rever.cyou/pub-img/opengraph_cover.png"],
+  },
+  metadataBase: new URL("https://rever.cyou"),
 };
 
 export default function RootLayout({
